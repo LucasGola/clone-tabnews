764d8e3e8a23005f86cdd3f93de7036e9bd9335f
Lucas Gola da Silva <lucasgola@outlook.com>
2026-05-21 13:49:37 -0300

    feat: connection with db

---
+++ infra/database.js
@@ -0,0 +1,25 @@
+import pg from "pg";
+
+const { Client } = pg;
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  await client.connect();
+
+  try {
+    return await client.query(queryObject);
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query,
+};
